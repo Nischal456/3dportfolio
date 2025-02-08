e9fc954518aba0fa749be281bfea092441139dfa
Shrestha Nischal <134735586+Nischal456@users.noreply.github.com>
2025-02-08 12:05:42 +0545

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -97,7 +97,7 @@ const Navbar = () => {
               ))}
               {/* "Connect with Me" Button for Mobile */}
               <li>
-                <Link to="/contact">
+                <Link to="/Cv/cv.pdf">
                   <button className="w-full bg-white text-primary font-bold px-4 py-2 rounded-lg hover:bg-gray-300 transition">
                     Connect with Me
                   </button>
