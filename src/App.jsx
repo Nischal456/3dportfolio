b94635f27a767ba49d46f0953bd3c9e683ee0bde
Shrestha Nischal <134735586+Nischal456@users.noreply.github.com>
2025-03-08 13:46:52 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 import Footer from "./components/Footer";
-import Portfolio from "./components/portfolio";
+import { Portfolio } from "./components";
 
 const App = () => {
   return (
