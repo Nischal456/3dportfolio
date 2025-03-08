21116a88d4ffa8b1f9b6664bbdd875f6d58c9912
Shrestha Nischal <134735586+Nischal456@users.noreply.github.com>
2025-03-08 13:43:45 +0545

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -7,7 +7,7 @@ import Experience from './Experience';
 import Works from './Works';
 import Feedbacks from './Feedbacks';
 import Contact from './Contact';
-import portfolio from './portfolio';
+import Portfolio from './Portfolio';
 
 export {
   Hero,
