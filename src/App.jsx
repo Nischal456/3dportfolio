f5f9b9333894cc6ae8e6b52381876cfd93f47cfa
Shrestha Nischal <134735586+Nischal456@users.noreply.github.com>
2025-05-01 19:22:38 +0545

    whatsapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,56 @@
 import { BrowserRouter } from "react-router-dom";
-
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas , Portfolio } from "./components";
+import { FaWhatsapp } from "react-icons/fa";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+  Portfolio,
+} from "./components";
 import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <div>
+            <a
+              href="https://wa.me/+9822790665" // Replace with your number
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{
+                position: "fixed",
+                bottom: "20px",
+                right: "20px",
+                backgroundColor: "#25D366",
+                color: "white",
+                borderRadius: "50%",
+                width: "60px",
+                height: "60px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                boxShadow: "0 2px 5px rgba(0,0,0,0.3)",
+                zIndex: 1000,
+                textDecoration: "none",
+                fontSize: "30px",
+              }}
+            >
+              <FaWhatsapp />
+            </a>
+          </div>
           <Navbar />
           <Hero />
         </div>
         <About />
         <Experience />
-        <div className='relative z-0'>
+        <div className="relative z-0">
           <Portfolio />
           <Contact />
           <StarsCanvas />
@@ -22,6 +59,6 @@ const App = () => {
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
